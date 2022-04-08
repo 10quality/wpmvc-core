14c81f99e260ebb8f471e106133ec2e818837ea3
Goi Garg <goigarg1@gmail.com>
2022-04-08 18:05:13 +0530

    Update Request.php

--- src/psr4/Request.php
+++ src/psr4/Request.php
@@ -122,7 +122,7 @@ class Request
         }
         return $value;
     }
-     /**
+   /**
      * Validate Request
      * @param array $items
      */
@@ -134,7 +134,7 @@ class Request
 
             //Check for Rules
             if (is_string($rules)) {
-                $rules = explode('|', $rules);
+                $rules = (explode('|', trim($rules)));
             }
 
             $value = self::input($item);
@@ -145,12 +145,27 @@ class Request
                 switch ($rule) {
                     case 'required':
                         if (empty($value)) {
-                            $response->error($item, "$itemCaps is required");
+                            $response->error($item, "$itemCaps is Required");
                         }
                         break;
                     case 'numeric':
                         if (!is_numeric($value)) {
-                            $response->error($item, "$itemCaps must be a number number");
+                            $response->error($item, "$itemCaps must be a Number");
+                        }
+                        break;
+                    case 'alphabetic':
+                        if (!ctype_alpha(str_replace(array(' ', "'", '-'), '', $value))) {
+                            $response->error($item, "$itemCaps contains illegal characters. Only alphabetic letters A-Z, \"'\", \" \" and \"-\" are allowed");
+                        }
+                        break;
+                    case 'alphanumeric':
+                        if (!ctype_alnum($value)) {
+                            $response->error($item, "<b>{$itemCaps}</b> contains illegal characters. Only alphabetic and numeric characters (A-Z and 0-9) are allowed");
+                        }
+                        break;
+                    case 'email':
+                        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                            $response->error($item, "$value must be a valid email address");
                         }
                         break;
                 }
